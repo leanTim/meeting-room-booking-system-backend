b09ee37d1d1f87e5db51b26d2865c11339897b0b
leanTim <zhu3521953@163.com>
2024-04-23 21:57:20 +0800

    register api start

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterUserDto } from './dto/register-user';
+
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    console.log('RegisterUser :>> ', registerUser);
+    return 'success'    
   }
 
   @Get()
